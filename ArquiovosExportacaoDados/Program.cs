b729bd68cb2db7e0f99828ba4ef57ef75f74fb43
Carlos Eduardo <ti.carloseduardo@gmail.com>
2025-05-29 12:26:37 -0300

    Ler um arquivo vendas e totalizar em outro Arquivo

--- ArquiovosExportacaoDados/Program.cs
+++ ArquiovosExportacaoDados/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using ArquiovosExportacaoDados.Entities;
 
 namespace ArquivosExpDados
     {
@@ -7,19 +9,32 @@ namespace ArquivosExpDados
         {
         static void Main(string[] args) {
 
-            string filePath = @"c:\temp\arquivo.txt";
-            string targetPath = @"c:\temp\arquivo2.txt";
-            //string filePath = @"c:\temp\arquivo.txt";//Exeptions tratada
+            Console.WriteLine("Digite o caminho do arquivo de origem:");
+            string filePath = Console.ReadLine();
 
             try
                 {
                 string[] lines = File.ReadAllLines(filePath);
+                
+                string sourceFolderPath = Path.GetDirectoryName(filePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\resumo.csv";
 
-                using (StreamWriter sw = File.AppendText(targetPath))
+                Directory.CreateDirectory(targetFolderPath); // Cria o diretório de destino se não existir
+
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
                     {
                     foreach (string line in lines)
                         {
-                        sw.WriteLine(line.ToUpper());
+                        string[] fields = line.Split(',');
+                        string descProduct = fields[0];
+                        double precoUnit = double.Parse(fields[1],CultureInfo.InvariantCulture);
+                        int quantidProduct = int.Parse(fields[2]);
+
+                        Product prod = new Product(descProduct, precoUnit, quantidProduct);
+
+                        sw.WriteLine(prod.DescProcuct + "," + prod.TotalPreco().ToString("F2", CultureInfo.InvariantCulture));
 
                         }
                     }
@@ -33,32 +48,3 @@ namespace ArquivosExpDados
 }
 
 
-/*
- *     class Program
-        {
-        static void Main(string[] args) {
-
-            string filePath = @"c:\temp\arquivo.txt";
-            string targetPath = @"c:\temp\arquivo2.txt";
-            //string filePath = @"c:\temp\arquivo.txt";//Exeptions tratada
-
-            try
-                {
-                //using (FileStream fs = new FileStream(filePath, FileMode.Open)){
-                //  using (StreamReader sr = new StreamReader(fs)) { 
-                using (StreamReader sr = File.OpenText(filePath)) {
-                    string line;
-                    while (!sr.EndOfStream)
-                        {
-                        line = sr.ReadLine();
-                        Console.WriteLine(line);
-                        }
-                    }
-                }
-            //}//modo antigo do FileStream StreamReader 
-            catch (Exception ex){
-                        Console.WriteLine("Ocorreu um erro ao ler o arquivo: " + ex.Message);
-            }
-        }
-    }
-*/
